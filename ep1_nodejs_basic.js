57775f88048e5dc9780a285ae8440641c1ef50cf
Thitphavanh <thitphavanh@gmail.com>
2022-09-18 21:28:41 +0700

    none blocking I/O

---
+++ ep1_nodejs_basic.js
@@ -0,0 +1,5 @@
+const message = "hery";
+console.log(message);
+
+const result = 5 + 5;
+console.log(result);
---
+++ ep2_none_blocking_IO_basic.js
@@ -0,0 +1,7 @@
+const message = "hery";
+console.log(message);
+
+const result = 5 + 5;
+console.log(result);
+
+
